124f378ebc373306dc241af271eaa4599ed0d4fe
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-27 09:25:45 -0600

    test for museum existance

---
+++ test/museum_test.rb
@@ -0,0 +1,19 @@
+
+require "minitest/autorun"
+require "./lib/museum"
+require "./lib/patron"
+require "./lib/exhibit"
+
+class MuseumTest < Minitest::Test
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+  end
+
+  def test_museum_exist
+    assert_instance_of Museum, @dmns
+  end
+
+  def test_museum_has_attributes
+    assert_equal "Denver Museum of Nature and Science", @dmns.name
+  end
+end
